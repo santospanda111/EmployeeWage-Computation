ecba9f1ff8167ed07cd5cd193833c49245e13e2d
santospanda111 <santospanda111@gmail.com>
2021-05-10 21:17:29 +0530

    Added UC8 code to calculate emp wage for multiple companies using parameterised static method

--- EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
+++ EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
@@ -3,20 +3,23 @@ package com.employeewagebuilder;
 public class CompanyEmpWage {
 	
     /**
-     * Refactored the whole code.
-     */
-    int partTimeEmpHr=4;
-    int empRatePerHr = 20;
-    int fullTimeEmpHrs = 8;
-    int empWage=0,totalMonthlyWage=0;
-    int totalWorkingHrs=0,totalWorkingDays=0;
-    /**
      * Put the while loop inside a method.
      * This while loop will iterate till working days is 20 and Total working hrs is 100.
      * calculate the monthly wage of Employee.
      */
-    void calculateTotalwage() {
-        while (totalWorkingDays < 20 && totalWorkingHrs < 100) {
+	/**
+	 * Created a method having these parameters through which it will calculate.
+	 * @param company
+	 * @param empRatePerHr
+	 * @param numOfWorkingDays
+	 * @param maxHoursPerMonth
+	 */
+   static void calculateTotalwage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth) {
+	    int partTimeEmpHr=4;
+	    int fullTimeEmpHrs = 8;
+	    int empWage=0,totalMonthlyWage=0;
+	    int totalWorkingHrs=0,totalWorkingDays=0;
+        while (totalWorkingDays < numOfWorkingDays && totalWorkingHrs < maxHoursPerMonth) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
 
@@ -41,17 +44,17 @@ public class CompanyEmpWage {
             }
             totalMonthlyWage = totalMonthlyWage + empWage;
         }
+        System.out.println("Company name: "+company);
         System.out.println("Employee Monthly Wage is :" + totalMonthlyWage);
     }
     /**
-     * inside the main method i have created a object employee.
-     * through that object i have called calculateTotalwage method.
+     * Here this method will take the parameters as input.
      * @param args
      */
     public static void main(String [] args) {
         System.out.println("Welcome To Employee wage Computation Program");
-        CompanyEmpWage employee = new CompanyEmpWage();
-        employee.calculateTotalwage();
+		calculateTotalwage("DMart", 20, 20, 100);
+		calculateTotalwage("Reliance", 30, 15, 150);
 	}
 
 }
