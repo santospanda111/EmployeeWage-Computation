0e096afd4e7964f144d3b9d20ade67a308180dd9
santospanda111 <santospanda111@gmail.com>
2021-05-10 20:12:52 +0530

    Added Uc1 Code to check employee is present or absent

--- EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
+++ EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
@@ -5,6 +5,15 @@ public class CompanyEmpWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To EmployeeWage Computation Program");
+		  /**
+         * This condition will check Employee is present or Absent.
+         */
+        int IS_PRESENT=1;
+        int empCheck =(int) Math.floor(Math.random()*10)%2;
+        if(empCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
 
 	}
 
