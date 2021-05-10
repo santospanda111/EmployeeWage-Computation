d3806e8447bd3a8a740b9fb92c1ffc95a66d8519
santospanda111 <santospanda111@gmail.com>
2021-05-10 20:31:20 +0530

    Added UC4 code solved by using Switch case

--- EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
+++ EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
@@ -5,32 +5,34 @@ public class CompanyEmpWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To EmployeeWage Computation Program");
-        /**
-         * This condition will check Employee is present or Absent.
-         * Here i have used Random function to generate random numbers.
-         */
-        int IS_PRESENT=1;
-        int partTimeEmpHr=4,partTime=2;
-        int empRatePerHr = 20;
-        int fullTimeEmpHrs = 8;
-        int empCheck =(int) Math.floor(Math.random()*10)%3;
-        /**
-         * This will calculate the daily wage of Employee.
-         */
-        if(empCheck == IS_PRESENT) {
-            System.out.println("Employee is Present");
-            System.out.println("Employee Daily Wage is :" + Math.multiplyExact(fullTimeEmpHrs, empRatePerHr));
-        }
-        /**
-         * This will calculate part-time wage of Employee
-         */
-        else if(empCheck==partTime){
-            System.out.println("Employee is Present but Half-Time");
-            System.out.println("Employee's Part-Time Wage is :" +Math.multiplyExact(partTimeEmpHr,empRatePerHr));
-        }else{
-            System.out.println("Employee is Absent");
-        }
-
+		/**
+		 * Here i have used Random function to generate random numbers.
+		 */
+		int IS_PRESENT=1;
+		int partTimeEmpHr=4,partTime=2;
+		int empRatePerHr = 20;
+		int fullTimeEmpHrs = 8;
+		int empCheck =(int) Math.floor(Math.random()*10)%3;
+		/**
+		 * This switch case will calculate the daily wage,Part-time wage of Employee.
+		 */
+		int attendance=empCheck;
+		
+		switch(attendance) {
+		
+		    case 0:
+		        System.out.println("Employee is absent");
+		    break;
+		
+			case 1:
+			    System.out.println("Employee is Present");
+			System.out.println("Employee Daily Wage is :" + Math.multiplyExact(fullTimeEmpHrs, empRatePerHr));
+			    break;
+			case 2:
+			    System.out.println("Employee is Present but Half-Time");
+			System.out.println("Employee's Part-Time Wage is :" + Math.multiplyExact(partTimeEmpHr, empRatePerHr));
+			        break;
+		}
 	}
 
 }
