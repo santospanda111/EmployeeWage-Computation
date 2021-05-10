d9ef7f3b5b9ac6c177bdb2a9b213f598e4f35ab5
santospanda111 <santospanda111@gmail.com>
2021-05-10 20:19:01 +0530

    Added Uc2 code to calculate Daily wage of employee.

--- EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
+++ EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
@@ -5,8 +5,9 @@ public class CompanyEmpWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To EmployeeWage Computation Program");
-		  /**
+        /**
          * This condition will check Employee is present or Absent.
+         * Here i have used Random function to generate random numbers.
          */
         int IS_PRESENT=1;
         int empCheck =(int) Math.floor(Math.random()*10)%2;
@@ -14,6 +15,15 @@ public class CompanyEmpWage {
             System.out.println("Employee is Present");
         else
             System.out.println("Employee is Absent");
+        /**
+         * This will calculate the daily wage of Employee.
+         */
+        int empRatePerHr = 20;
+        int empHrs = 8;
+        if( IS_PRESENT == empCheck)
+            System.out.println("Employee Daily Wage is :" +Math.multiplyExact(empHrs,empRatePerHr));
+        else
+            System.out.println("Employee Daily Wage is : 0");
 
 	}
 
