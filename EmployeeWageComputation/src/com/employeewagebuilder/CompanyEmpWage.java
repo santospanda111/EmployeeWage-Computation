4c19e41272fe18da2f9c0ffda61a0898aa153257
santospanda111 <santospanda111@gmail.com>
2021-05-10 20:07:24 +0530

    Added Welcome message

---
+++ EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
@@ -0,0 +1,11 @@
+package com.employeewagebuilder;
+
+public class CompanyEmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To EmployeeWage Computation Program");
+
+	}
+
+}
