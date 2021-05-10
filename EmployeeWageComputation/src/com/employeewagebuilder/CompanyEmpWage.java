d2d38effc7e93bd5914a18083d320097656cbdfc
santospanda111 <santospanda111@gmail.com>
2021-05-10 20:37:43 +0530

    Added UC5 code to calculate monthly wage of Employee.

--- EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
+++ EmployeeWageComputation/src/com/employeewagebuilder/CompanyEmpWage.java
@@ -4,35 +4,40 @@ public class CompanyEmpWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome To EmployeeWage Computation Program");
-		/**
-		 * Here i have used Random function to generate random numbers.
-		 */
-		int IS_PRESENT=1;
-		int partTimeEmpHr=4,partTime=2;
-		int empRatePerHr = 20;
-		int fullTimeEmpHrs = 8;
-		int empCheck =(int) Math.floor(Math.random()*10)%3;
-		/**
-		 * This switch case will calculate the daily wage,Part-time wage of Employee.
-		 */
-		int attendance=empCheck;
-		
-		switch(attendance) {
-		
-		    case 0:
-		        System.out.println("Employee is absent");
-		    break;
-		
-			case 1:
-			    System.out.println("Employee is Present");
-			System.out.println("Employee Daily Wage is :" + Math.multiplyExact(fullTimeEmpHrs, empRatePerHr));
-			    break;
-			case 2:
-			    System.out.println("Employee is Present but Half-Time");
-			System.out.println("Employee's Part-Time Wage is :" + Math.multiplyExact(partTimeEmpHr, empRatePerHr));
-			        break;
-		}
+		System.out.println("Welcome To EmployeeWage Computation Program");  
+        int IS_PRESENT=1;
+        int partTimeEmpHr=4,partTime=2;
+        int empRatePerHr = 20;
+        int fullTimeEmpHrs = 8;
+        int empWage=0,totalMonthlyWage=0;
+        int totalEmpDays=20;
+        /**
+         * This for loop will iterate and calculate the monthly wage of Employee.
+         * Here i have used Random function to generate random numbers.
+         */
+        for (int i=0; i<totalEmpDays; i++) {
+             int empCheck =(int) Math.floor(Math.random()*10)%3;
+             int attendance=empCheck;
+             switch (attendance) {
+
+                case 0:
+                    System.out.println("Employee is absent");
+                    break;
+
+                case 1:
+                    System.out.println("Employee is Present");
+                    empWage = Math.multiplyExact(fullTimeEmpHrs, empRatePerHr);
+                    System.out.println("Employee Daily Wage is :" + empWage);
+                    break;
+                case 2:
+                    System.out.println("Employee is Present but Half-Time");
+                    empWage = Math.multiplyExact(partTimeEmpHr, empRatePerHr);
+                    System.out.println("Employee's Part-Time Wage is :" + empWage);
+                    break;
+            }
+            totalMonthlyWage = totalMonthlyWage + empWage;
+        }
+        System.out.println("Employee Monthly Wage is :" + totalMonthlyWage);
 	}
 
 }
